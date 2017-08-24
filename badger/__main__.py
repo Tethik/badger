ddb7fd4cf2072389b9c8b0594d2959340d665fc6
Joakim Uddholm <joakim@uddholm.com>
2017-08-24 23:20:43 +0200

    Python2 fix.

--- badger/__main__.py
+++ badger/__main__.py
@@ -70,7 +70,7 @@ def main(argv=None):
         if not args.quiet:
             print('Saved badge to {}'.format(path))
     else:
-        print(badge, end='')
+        print(badge)
 
 if __name__ == '__main__':
     main()
