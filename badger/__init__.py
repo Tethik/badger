96d05c9039fda39d267af4fb62c938b16b331a90
Joakim Uddholm <joakim@uddholm.com>
2017-08-24 23:17:13 +0200

    Refactored into classes. Added more options.

--- badger/__init__.py
+++ badger/__init__.py
@@ -1,5 +1,5 @@
 """
-Generate coverage badges for Coverage.py.
+Code for generating different types of repo badges in SVG form.
 """
 # -*- coding: utf-8 -*-
 from __future__ import print_function, division, absolute_import, unicode_literals
@@ -10,7 +10,6 @@ import argparse
 import pkg_resources
 
 __version__ = '0.0.1'
-
 
 DEFAULT_COLOR = '#a4a61d'
 COLORS = {
@@ -32,127 +31,100 @@ COLOR_RANGES = [
     (0, 'red'),
 ]
 
-def get_color(total):
+
+class Badge(object):
     """
-    Return color for current coverage precent
+    Class that generates the SVG for a single badge in the following format:
+        label | value
+    label and value can be any string.
     """
-    try:
-        xtotal = int(total)
-    except ValueError:
-        return COLORS['lightgrey']
-    for range_, color in COLOR_RANGES:
-        if xtotal >= range_:
-            return COLORS[color]
 
-def get_percentage_badge(key, percentage):
+    def __init__(self, label, value, color=DEFAULT_COLOR):
+        self.label = label
+        self.value = value
+        self.color = color
+
+    def render(self):
+        """
+        Read the SVG template from the package, update total, return SVG as a
+        string.
+        """
+        label_width = max(80, 10 + 9 * len(self.label))
+        value_width = max(36, 10 + 9 * len(str(self.value)))
+        width = max(99, label_width + value_width)
+        value_x = label_width + (value_width / 2)
+        template_path = os.path.join('templates', 'flat.svg')
+        template = pkg_resources.resource_string(__name__, template_path).decode('utf8')
+
+        return template\
+            .replace('{{ label }}', self.label)\
+            .replace('{{ color }}', self.color)\
+            .replace('{{ value }}', str(self.value))\
+            .replace('{{ width }}', str(width))\
+            .replace('{{ value_x }}', str(value_x))\
+            .replace('{{ label_width }}', str(label_width))\
+            .replace('{{ label_x }}', str(int(label_width / 2)))\
+            .replace('{{ value_width }}', str(value_width))
+
+    def save(self, filepath, force=False):
+        """
+        Save badge to the specified path.
+        """
+        # Validate path (part 1)
+        if filepath.endswith('/'):
+            raise IOError('Error: Filepath may not be a directory.')
+
+        # Get absolute filepath
+        path = os.path.abspath(filepath)
+        if not path.lower().endswith('.svg'):
+            path += '.svg'
+
+        # Validate path (part 2)
+        if not force and os.path.exists(path):
+            raise FileExistsError('Error: "{}" already exists.'.format(path))
+
+        # Write file
+        with open(path, 'w') as _file:
+            _file.write(self.render())
+
+        return path
+
+    def __str__(self):
+        return self.render()
+
+class ColorRangeBadge(Badge):
     """
-    Return badge with automatic color chosen based on percentage (100% = green, 0% = red)
+    A badge that automatically picks a color between red and green based on a
+    numeric value.
     """
-    return get_badge(key, str(int(percentage)) + "%", color=get_color(percentage))
 
-def get_badge(key, value, color=DEFAULT_COLOR):
+    def __init__(self, label, numeric_value, minimum=0, maximum=100):
+        super(ColorRangeBadge, self).__init__(label, numeric_value)
+        self.minimum = minimum
+        self.maximum = maximum        
+        self.color = self.get_color(numeric_value)
+        
+
+    def get_color(self, total):
+        """
+        Return color for total relative to minimum and maximum of range.
+        """
+        try:
+            xtotal = int(total)
+        except ValueError:
+            return COLORS['lightgrey']
+
+        factor = 100 / (self.maximum - self.minimum)
+        print(factor)
+        for _range, color in COLOR_RANGES:
+            if xtotal >= _range * factor:
+                return COLORS[color]
+
+class PercentageBadge(ColorRangeBadge):
     """
-    Read the SVG template from the package, update total, return SVG as a
-    string.
+    Badge that takes a percentage value between 0-100 and automatically renders with a relative red to green color range.
     """
-    key_width = max(80, 10 + 9 * len(key))
-    value_width = max(36, 10 + 9 * len(value))
-    # print(key_width)
-    # print(value_width)
-    width = max(99, key_width + value_width)
-    value_x = key_width + (value_width / 2)
-    template_path = os.path.join('templates', 'flat.svg')
-    template = pkg_resources.resource_string(__name__, template_path).decode('utf8')
 
-    return template\
-      .replace('{{ key }}', key)\
-      .replace('{{ color }}', color)\
-      .replace('{{ value }}', value)\
-      .replace('{{ width }}', str(width))\
-      .replace('{{ value_x }}', str(value_x))\
-      .replace('{{ key_width }}', str(key_width))\
-      .replace('{{ key_x }}', str(int(key_width / 2)))\
-      .replace('{{ value_width }}', str(value_width))
-
-
-def parse_args(argv=None):
-    """
-    Parse the command line arguments.
-    """
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('key')
-    parser.add_argument('value')
-
-    # parser.add_argument('--max', dest='max',
-    #                     help='Max value')
-    # parser.add_argument('--min', dest='min',
-    #                     help='Min value')
-
-    parser.add_argument('-o', dest='filepath',
-                        help='Save the file to the specified path.')
-    parser.add_argument('-p', dest='plain_color', action='store_true',
-                        help='Plain color mode. Standard green badge.')
-    parser.add_argument('-f', dest='force', action='store_true',
-                        help='Force overwrite image, use with -o key.')
-    parser.add_argument('-q', dest='quiet', action='store_true',
-                        help='Don\'t output any non-error messages.')
-    parser.add_argument('-v', dest='print_version', action='store_true',
-                        help='Show version.')
-
-    # If arguments have been passed in, use them.
-    if argv:
-        return parser.parse_args(argv)
-
-    # Otherwise, just use sys.argv directly.
-    return parser.parse_args()
-
-
-def save_badge(badge, filepath, force=False):
-    """
-    Save badge to the specified path.
-    """
-    # Validate path (part 1)
-    if filepath.endswith('/'):
-        print('Error: Filepath may not be a directory.')
-        sys.exit(1)
-
-    # Get absolute filepath
-    path = os.path.abspath(filepath)
-    if not path.lower().endswith('.svg'):
-        path += '.svg'
-
-    # Validate path (part 2)
-    if not force and os.path.exists(path):
-        print('Error: "{}" already exists.'.format(path))
-        sys.exit(1)
-
-    # Write file
-    with open(path, 'w') as _file:
-        _file.write(badge)
-
-    return path
-
-
-def main(argv=None):
-    """
-    Console scripts entry point.
-    """
-    args = parse_args(argv)
-
-    # Print version
-    if args.print_version:
-        print('badger v{}'.format(__version__))
-        sys.exit(0)
-
-
-    percentage = args.value
-    color = DEFAULT_COLOR if args.plain_color else get_color(percentage)
-    badge = get_badge(args.key, args.value, color)
-
-    # Show or save output
-    if args.filepath:
-        path = save_badge(badge, args.filepath, args.force)
-        if not args.quiet:
-            print('Saved badge to {}'.format(path))
-    else:
-        print(badge, end='')
+    def __init__(self, label, numeric_value):
+        super(PercentageBadge, self).__init__(label, numeric_value, 0, 100)
+        self.value = str(numeric_value) + '%'
--- badger/__main__.py
+++ badger/__main__.py
@@ -1,4 +1,76 @@
-from badger import main
+"""
+Console script to generate SVG badge from a commandline.
+"""
+
+import argparse
+import sys
+import badger
+
+
+def parse_args(argv=None):
+    """
+    Parse the command line arguments.
+    """
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('key')
+    parser.add_argument('value')
+
+    # parser.add_argument('--max', dest='max',
+    #                     help='Max value')
+    # parser.add_argument('--min', dest='min',
+    #                     help='Min value')
+
+    parser.add_argument('-o', dest='filepath',
+                        help='Save the file to the specified path.')
+
+    parser.add_argument('-p', dest='percentage_mode', action='store_true',
+                        help='Percentage mode. Standard green badge.')
+    parser.add_argument('-s', dest='simple_mode', action='store_true',
+                        help='Simple mode. Standard green badge unless given by -c')
+    parser.add_argument('-c', dest='color', action='store_true',
+                        help='Sets the color of the badge. Must be in hex, e.g. #a4a61d')
+
+    parser.add_argument('-f', dest='force', action='store_true',
+                        help='Force overwrite image, use with -o key.')
+    parser.add_argument('-q', dest='quiet', action='store_true',
+                        help='Don\'t output any non-error messages.')
+    parser.add_argument('-v', dest='print_version', action='store_true',
+                        help='Show version.')
+
+    # If arguments have been passed in, use them.
+    if argv:
+        return parser.parse_args(argv)
+
+    # Otherwise, just use sys.argv directly.
+    return parser.parse_args()
+
+
+
+def main(argv=None):
+    """
+    Console scripts entry point.
+    """
+    args = parse_args(argv)
+
+    # Print version
+    if args.print_version:
+        print('badger v{}'.format(badger.__version__))
+        sys.exit(0)
+
+    # Attempt to parse % type badge automatically.
+    if args.percentage_mode:
+        badge = badger.PercentageBadge(args.key, float(args.value.replace('%', '')))
+    else:
+        color = args.color or badger.DEFAULT_COLOR
+        badge = badger.Badge(args.key, args.value, color=color)
+
+    # Show or save output
+    if args.filepath:
+        path = badge.save(args.filepath, args.force)
+        if not args.quiet:
+            print('Saved badge to {}'.format(path))
+    else:
+        print(badge, end='')
 
 if __name__ == '__main__':
     main()
