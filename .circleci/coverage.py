88b45b3e1beb3f63e195027dd0fea2ffcb8ab204
Joakim Uddholm <joakim@uddholm.com>
2017-09-07 12:37:58 +0200

    [WIP] Circle integration (#6)
    * Try to add scripting to generate badges, using badger itself :)
    
    * Yaml fixes.
    
    * Add badges dir.
    
    * More fixes for circle.
    
    * More fixes.
    
    * fixed regex
    
    * Upload badges *after* creating them :P
    
    * Every mistake will be squashed.
    
    * Add badges to readme.
    
    * Install in editable/develop mode instead. Hoping this fixes code coverage reporting.

---
+++ .circleci/coverage.py
@@ -0,0 +1,17 @@
+import math
+import sys
+import re
+import badger
+
+
+report_file, svg_file = sys.argv[1], sys.argv[2]
+
+regex = re.compile(r'branch-rate="(\d+\.?\d*)%?"', re.MULTILINE)
+
+text = open(report_file).read()
+
+match = re.search(regex, text)
+total_line_precent = float(match.group(1)) * 100
+print(total_line_precent)
+badge = badger.PercentageBadge('coverage', total_line_precent)
+badge.save(svg_file)
