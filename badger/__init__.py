588db063f191f891ebdced7dd1643e02ed2a3f81
Joakim Uddholm <joakim@uddholm.com>
2018-01-24 00:55:15 +0100

    chore(*): lint fixes.

--- badger/__init__.py
+++ badger/__init__.py
@@ -40,6 +40,25 @@ COLOR_RANGES = [
 FONT = "DejaVuSans.ttf"
 FONT_SIZE = 11
 
+def _calculate_width_of_text(text):
+    """
+    Calculate the actual pixel width of a text
+    """
+
+    font_path = os.path.join('fonts', FONT)
+    _file = pkg_resources.resource_filename(__name__, font_path)
+    face = freetype.Face(_file)
+
+    face.set_char_size(FONT_SIZE*64)
+    previous = 0
+    width = 0
+    for character in text:
+        face.load_char(character)
+        kerning = face.get_kerning(previous, character)
+        width += face.glyph.advance.x + kerning.x
+    logging.debug('Freetype calculated width %i for text "%s"', width >> 6, text)
+    return width >> 6
+
 
 class Badge(object):
     """
@@ -48,34 +67,13 @@ class Badge(object):
     label and value can be any string.
     """
 
-    def __init__(self, label, value, value_color=DEFAULT_VALUE_COLOR, label_color=DEFAULT_LABEL_COLOR):
+    def __init__(self, label, value,
+                 value_color=DEFAULT_VALUE_COLOR,
+                 label_color=DEFAULT_LABEL_COLOR):
         self.label = label
         self.value = value
         self.label_color = label_color
         self.value_color = value_color
-
-    def _calculate_width_of_text(self, text):
-        """
-        Calculate the actual pixel width of a text
-        """
-        # try:
-        
-        font_path = os.path.join('fonts', FONT)
-        _file = pkg_resources.resource_filename(__name__, font_path)
-        face = freetype.Face(_file)
-
-        face.set_char_size(FONT_SIZE*64)
-        previous = 0
-        width = 0
-        for character in text:
-            face.load_char(character)
-            kerning = face.get_kerning(previous, character)
-            width += face.glyph.advance.x + kerning.x
-        logging.debug('Freetype calculated width %i for text "%s"', width >> 6, text)        
-        return (width >> 6)
-        # except:
-        #     print("Used estimated width")
-        #     return 10 + 6 * len(text)
 
 
     def render(self):
@@ -86,10 +84,10 @@ class Badge(object):
 
         padding_outside = 6.0
         padding_inside = 4.0
-        label_text_width = self._calculate_width_of_text(self.label)
-        value_text_width = self._calculate_width_of_text(str(self.value))
-        
-        args = {           
+        label_text_width = _calculate_width_of_text(self.label)
+        value_text_width = _calculate_width_of_text(str(self.value))
+
+        args = {
             'value': str(self.value),
             'label': self.label,
             'value_color': self.value_color,
@@ -122,6 +120,7 @@ class Badge(object):
     def __str__(self):
         return self.render()
 
+
 class ColorRangeBadge(Badge):
     """
     A badge that automatically picks a color between red and green based on a
@@ -131,9 +130,9 @@ class ColorRangeBadge(Badge):
     def __init__(self, label, numeric_value, minimum=0, maximum=100):
         super(ColorRangeBadge, self).__init__(label, numeric_value)
         self.minimum = minimum
-        self.maximum = maximum        
+        self.maximum = maximum
         self.color = self.get_color(float(numeric_value))
-        
+
 
     def get_color(self, total):
         """
@@ -142,14 +141,16 @@ class ColorRangeBadge(Badge):
         xtotal = int(total)
 
         factor = 100 / (self.maximum - self.minimum)
-        print(factor)
         for _range, color in COLOR_RANGES:
             if xtotal >= _range * factor:
                 return COLORS[color]
+        return COLORS['red']
+
 
 class PercentageBadge(ColorRangeBadge):
     """
-    Badge that takes a percentage value between 0-100 and automatically renders with a relative red to green color range.
+    Badge that takes a percentage value between 0-100 and automatically renders
+    with a relative red to green color range.
     """
 
     def __init__(self, label, numeric_value):
